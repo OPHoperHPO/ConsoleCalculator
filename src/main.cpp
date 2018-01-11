7cb35b1804c8448f11227b9ef25b7466ddddd3b3
Anodev <29470622+OPHoperHPO@users.noreply.github.com>
2018-01-11 15:48:23 +1000

    v1.0.2_unstable

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <iostream>
 int x;
 int y;
@@ -6,9 +7,17 @@ int console;
 #include <time.h>
 using namespace std;
 
+int factorial(int x) {
+	if (x == 1) {
+		return 1;
+	}
+	else {
+		return x * factorial(x - 1);
+	}
+}
 int main() {
 	cout << "Calculator rev1.0\n";
-	cout << "Write command.\nFor help write '5'\n\n\nroot@calculator-unstable:~# ";
+	cout << "Write command.\nFor help write '5'\nDon't TRY TO WRITE WORDS!!!\nONLY NUMBERS!!!\nLike:help and other...\n\n\nroot@calculator-unstable:~# ";
 	cin >> console;
 	if (console == 1) {
 		cout << "Write number\n";
@@ -47,11 +56,19 @@ int main() {
 		main();
 	}
 	if (console == 5) {
-		cout << "Commands\n 1 - Sum\n 2 - Multiplication Y\n 3 - Subtraction\n 4 - Division\n 5 - Help\n 6 - Close\n Ctrl+C - Emergency stop\n";
+		cout << "Commands\n 1 - Sum\n 2 - Multiplication Y\n 3 - Subtraction\n 4 - Division\n 5 - Help\n 6 - Close\n 7 - Factorial\n Ctrl+C - Emergency stop\n";
 		main();
 	}
 	if (console == 6) {
 		exit(0);
+	}
+	if (console == 7) {
+		cout << "Write number\n !";
+		cin >> x;
+		
+		if (factorial(x) == 0) { cout << "Too many numbers...\n"; }
+		else{cout << "Factorial:" << factorial(x) << " \n"; }
+		main();
 	}
 	else {
 		cout << "Error Command\n";
